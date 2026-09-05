353b21b175ae1a1c0d8dd778da77c11b4e96d4d0
bautistafailo <bautistafailo@hotmail.com>
2026-09-05 20:22:59 -0300

    Arreglo bug de seller en Product y confirmo filtro por vendedor

--- demo/src/main/java/com/uade/tpo/demo/entity/Product.java
+++ demo/src/main/java/com/uade/tpo/demo/entity/Product.java
@@ -1,5 +1,7 @@
 package com.uade.tpo.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -35,6 +37,7 @@ public class Product {
     private Category category;
 
     // Usuario vendedor que publicó el producto
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "seller_id", referencedColumnName = "id")
     private User seller;
